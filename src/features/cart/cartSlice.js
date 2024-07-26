7af775dcc9f0414e7e82e2eff73793c4a2eec77e
sahil khatri <sahilkeshav1997@gmail.com>
2024-07-26 14:14:10 +0530

    data fetching

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit'
-import cartItems from '../../cartItems'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-  cartItems,
+  cartItems: [],
   amount: 0,
   total: 0,
   isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () =>
+  fetch(url)
+    .then((resp) => resp.json())
+    .catch((error) => console.log(error))
+)
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -41,6 +48,19 @@ const cartSlice = createSlice({
       state.amount = amount
       state.total = total
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.cartItems = action.payload
+      })
+      .addCase(getCartItems.rejected, (state) => {
+        state.isLoading = false
+      })
   },
 })
 
